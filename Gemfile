52ca0af1428b879261703b8040f983c10fdd37ae
shaomingtan <t.shaoming@gmail.com>
2012-11-01 01:58:17 -0700

    updated user model wth name and password validation. Working on email uniqueness on database, experiencing error as cannot add unique index to email

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.5'
-
+gem 'bcrypt-ruby', '3.0.1'
 gem 'json'
 gem 'jquery-rails'
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,20 @@
 #
 # Table name: users
 #
-#  id         :integer          not null, primary key
-#  email      :string(255)
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id              :integer          not null, primary key
+#  email           :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  name            :string(255)
+#  password_digest :string(255)
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email
+  attr_accessible :email, :name
+
+  before_save { |user| user.email = email.downcase }
+  validates :name,  presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 end
