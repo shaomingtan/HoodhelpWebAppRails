8186464a24ecfe764747c639a7f1780bb4ebacd0
shaomingtan <t.shaoming@gmail.com>
2012-11-05 15:01:54 -0800

    added user sign up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,29 @@
 
 class UsersController < ApplicationController
 
+  def new
+    @user = User.new
+  end
+
   def create
     @user = User.new(params[:user])
-    
-   	@user.save
-    redirect_to root_path
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  	@user = User.find(params[:id])
+  end
+
+  def index
+  	@users = User.all()
+  	respond_to do |format|
+  		format.html
+  	end
   end
 
 end
